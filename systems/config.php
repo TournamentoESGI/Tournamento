dad0c60b6bd6f68edd15a112b7131878424aa6aa
hlemaire <h.lemaire@myskolae.fr>
2026-03-07 19:46:57 +0100

    update config.php

--- systems/config.php
+++ systems/config.php
@@ -1,12 +1,14 @@
 <?php
-include_once("./components/basics.php");
-include_once "./components/error.php";
+$rootDir = __DIR__ . '/../';
+
+include_once($rootDir . "components/basics.php");
+include_once($rootDir . "components/error.php");
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$env = parse_ini_file('.env');
+$env = parse_ini_file($rootDir . '.env');
 
 try {
     $dsn = sprintf("mysql:host=%s;dbname=%s;charset=utf8", $env['DB_HOST'], $env['DB_BASE']);
@@ -14,12 +16,14 @@ try {
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ];
+    
     $pdo = new PDO($dsn, $env['DB_USER'], $env['DB_PASSWORD'], $options);
-	include_once("./setup.php");
+    
+    include_once(__DIR__ . '/setup.php');
 
 } catch (Exception $ex) {
     error_log($ex->getMessage()); 
-	http_response_code(500);
-	display_page_error($ex->getMessage());
+    http_response_code(500);
+    display_page_error($ex->getMessage());
 }
-
+?>
