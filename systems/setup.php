41be35baeee88599fb257d2cfb50a0f63f8fd8e9
hlemaire <h.lemaire@myskolae.fr>
2026-03-24 18:55:58 +0100

    update sql

--- systems/setup.php
+++ systems/setup.php
@@ -1,6 +1,12 @@
 <?php
-$sql = "
-CREATE TABLE IF NOT EXISTS users (
+function createTable($table) {
+    global $pdo;
+    $query = "CREATE TABLE IF NOT EXISTS ".$table.";";
+    $pdo->exec($query);
+}
+
+createTable("
+users (
     id_users INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(20) NOT NULL UNIQUE,
     first_name VARCHAR(50) NOT NULL,
@@ -10,8 +16,28 @@ CREATE TABLE IF NOT EXISTS users (
     email_address VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     current_balance INT DEFAULT 0,
-    creation_date DATE
+    creation_date DATE DEFAULT CURRENT_DATE(),
+    profil_picture VARCHAR(255) DEFAULT 'default_profile_picture.png'
 );
+");
 
-";
-$pdo->exec($sql);
+createTable("
+tournaments (
+    id_tournaments INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50),
+    description VARCHAR(255),
+    games VARCHAR(50),
+    status ENUM('ouvert','fermer'),
+    created_at DATE DEFAULT CURRENT_DATE(),
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL
+);
+");
+
+createTable("
+pools(
+    id_pools INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50)
+);
+");
+?>
