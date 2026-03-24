1934a27e96aff8ffce91c288a107cda9f66cacc4
hlemaire <h.lemaire@myskolae.fr>
2026-03-24 11:17:37 +0100

    sql update

--- systems/setup.php
+++ systems/setup.php
@@ -6,10 +6,10 @@ CREATE TABLE IF NOT EXISTS users (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     date_of_birth DATE NOT NULL,
-    phone NUMBER(10) NOT NULL UNIQUE,
+    phone INT(10) NOT NULL UNIQUE,
     email_address VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    current_balance NUMBER DEFAULT 0,
+    current_balance INT DEFAULT 0,
     creation_date DATE DEFAULT CURRENT_DATE,
     profil_picture DEFAULT 'default_profile_picture.jpg'
 );
