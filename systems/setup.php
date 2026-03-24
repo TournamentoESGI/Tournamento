50c16c44d050ca0348bcaff753422cf4befd6836
hlemaire <h.lemaire@myskolae.fr>
2026-03-24 11:08:17 +0100

    sql update

--- systems/setup.php
+++ systems/setup.php
@@ -14,35 +14,5 @@ CREATE TABLE IF NOT EXISTS users (
     profil_picture DEFAULT 'default_profile_picture.jpg'
 );
 
-CREATE TABLE IF NOT EXISTS tournaments (
-    id_tournaments INT AUTO_INCREMENT PRIMARY KEY,
-    tournament_name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    discipline VARCHAR(50) NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    entry_fee NUMBER(10) NOT NULL,
-    max_users INT NOT NULL,
-    cash_prize NUMBER(10) NOT NULL,
-    bet_status ENUM('open', 'closed') NOT NULL DEFAULT 'open',
-    status ENUM('started', 'upcoming', 'completed') NOT NULL DEFAULT 'upcoming',
-    img DEFAULT 'default_tournament_image.jpg'
-);
-
-CREATE TABLE IF NOT EXISTS bets (
-    id_bets INT AUTO_INCREMENT PRIMARY KEY,
-    id_users INT NOT NULL,
-    id_tournaments INT NOT NULL,
-    stake_amount NUMBER(10) NOT NULL,
-    potential_win NUMBER(10) NOT NULL,
-    status ENUM('pending', 'won', 'lost') NOT NULL DEFAULT 'pending',
-    bet_target VARCHAR(255) NOT NULL,
-    bet_date DATE DEFAULT CURRENT_DATE,
-    FOREING KEY id_users REFERENCE users(id_users),
-    FOREING KEY id_tournaments REFERENCE tournaments(id_tournaments)
-);
-
 ";
-
 $pdo->exec($sql);
-?>
