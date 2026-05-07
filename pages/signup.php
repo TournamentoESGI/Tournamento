ba8b2165621b968f5f278d7d3dd1ffd9473f837f
CaporalTacos <anthogemu@gmail.com>
2026-05-07 05:09:44 +0200

    captcha added + responsive

--- pages/signup.php
+++ pages/signup.php
@@ -1,48 +1,52 @@
+<?php
+include_once("./components/captcha.php");
+?>
+
 <div class="signup-presentation">
     <div class="signup-carte">
         <div class="signup-carte-titre">
-            <h2>Inscription :</h2>
+            <h2>Inscription:</h2>
         </div>
         <div class="signup-form">
             <h2>Sign Up </h2>
             <form action="?page=signup" method="post">
                 <div class="mb-3">
-                    <label for="username">Nom d'utilisateur :</label>
+                    <label for="username">Nom d'utilisateur:</label>
                     <input type="text" name="username" placeholder="Ex. CaporalTacos..." required>
                 </div>
 
                 <div class="mb-3">
-                    <label for="first_name">Prénom :</label>
+                    <label for="first_name">Prénom:</label>
                     <input type="text" name="first_name" placeholder="Ex. Julia..." required>
                 </div>
 
                 <div class="mb-3">
-                    <label for="last_name">Nom :</label>
+                    <label for="last_name">Nom:</label>
                     <input type="text" name="last_name" placeholder="Ex. DUPONT..." required>
                 </div>
 
                 <div class="mb-3">
-                    <label for="email_address">Email :</label>
+                    <label for="email_address">Email:</label>
                     <input type="email" name="email_address" placeholder="Ex. adress@gmail.com..." required>
                 </div>
 
                 <div class="mb-3">
-                    <label for="date_of_birth">Date de naissance</label>
+                    <label for="date_of_birth">Date de naissance:</label>
                     <input type="date" id="date_of_birth" name="date_of_birth" max="<?= date('Y-m-d') ?>" required>
                 </div>
 
                 <div class="mb-3">
-                    <label for="phone">Numéro de téléphone</label>
+                    <label for="phone">Numéro de téléphone:</label>
                     <input type="tel" id="phone" name="phone" minlength="10" maxlength="10" placeholder="Ex. +33 7 68 65.." required>
                 </div>
                 
                 <div class="mb-3">
-                    <label for="password">Mot de Passe :</label>
+                    <label for="password">Mot de Passe:</label>
                     <input type="password" name="password" minlength="8" placeholder="Password.." required>
                 </div>
 
                 <div class="mb-3">
-                    <label for="passwordverify">Vérification Mot de Passe :</label>
+                    <label for="passwordverify">Vérification Mot de Passe:</label>
                     <input type="password" name="passwordverify" minlength="8" placeholder="Verify Password.." required>
                 </div>
 
@@ -52,7 +56,13 @@
                         Accepter les termes et conditions
                     </label>
                 </div>
-
+                
+                <div class="captcha-section">
+                        <p>CAPTCHA: Veuillez assembler cette image:</p>
+                        <?php
+                        generateCaptcha($pdo);
+                        ?>
+                </div>
                 <button type="submit" name="submit" class="btn-signup">Sign Up</button>
 
             </form>
@@ -64,8 +74,9 @@
         <p>Tournamento est la plateforme idéale pour organiser et regarder des tournois !
              Si vous êtes embitieux et souhaitez participer à un tournoi pour gagner de l'argent,
               n'hésitez pas et sautez dans l'arène ! 
-        </p>
-        <img src="assets/second_logo.png" alt="Logo" class="second-logo">
+            </p>
+            <img src="assets/second_logo.png" alt="Logo" class="second-logo" >
+
     
 
 
@@ -102,8 +113,8 @@ if (isset($_POST['submit'])) {
         echo "<p>Le mot de passe doit faire au moins 8 caractères.</p>";
         $count_error += 1;
 
-    } if ($age < 13) {
-        echo "<p>Vous devez avoir au moins 13 ans pour vous inscrire.</p>";
+    } if ($age < 16) {
+        echo "<p>Vous devez avoir au moins 16 ans pour vous inscrire.</p>";
         $count_error += 1;
 
     } if ($_POST['password']!= $_POST['passwordverify']) {
@@ -121,6 +132,11 @@ if (isset($_POST['submit'])) {
     } if (!isset($_POST['termconditions'])) {
         echo "<p>Vous devez accepter les termes et conditions</p>";
         $count_error += 1;
+
+    } if (!isCaptchaValid($_POST['captcha'])) {
+    echo "<p> Captcha Invalide </p>";
+    $count_error += 1;
+    
     }
 
     $sql= "SELECT * FROM users WHERE username = '$username' OR email_address = '$email_address'";
@@ -134,7 +150,9 @@ if (isset($_POST['submit'])) {
     }
 
     echo "</div>";
+    
     echo "<div class='success'>";
+
     if ($count_error==0) {
         $sql = "INSERT INTO users (username, first_name, last_name, email_address, password, date_of_birth, phone)
                 VALUES ('$username', '$first_name', '$last_name', '$email_address', '$password', '$date_of_birth', '$num_numeric')";
