1a1d82f16f21e4b351e0d7cc63f13bedbd15b7b2
Hugo Lemaire <h.lemaire@myskolae.fr>
2026-05-09 22:49:16 +0200

    T5 resolve mail2

--- components/mail.php
+++ components/mail.php
@@ -1,33 +1,34 @@
 <?php
-require './vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 function SendMail($email, $subject, $contenu) {
-    
+
+    $env = parse_ini_file(__DIR__ . '/../.env');
+
     $mail = new PHPMailer(true);
-    $env = parse_ini_file('.env');
-    
+
     try {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
         $mail->Username   = 'noreplytournamento@gmail.com';
-        $mail->Password = env['SMTP_PASSWORD'];
+        $mail->Password   = $env['SMTP_PASSWORD'];
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
-    
+
         $mail->setFrom('noreplytournamento@gmail.com', 'no-replytournamento');
         $mail->addAddress($email);
-    
+
         $mail->isHTML(true);
         $mail->Subject = $subject;
         $mail->Body    = $contenu;
-    
+
         $mail->send();
         echo 'Email envoyé !';
     } catch (Exception $e) {
         echo "Erreur : {$mail->ErrorInfo}";
     }
 }
-?>
