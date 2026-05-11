9eef41e7865c923dbf21311cbe676cab8dd87319
Hugo Lemaire <h.lemaire@myskolae.fr>
2026-05-11 22:04:45 +0200

    ajout insert captchas test.php

--- systems/tests.php
+++ systems/tests.php
@@ -9,6 +9,7 @@ makeDatabase();
 $password = password_hash("ez",PASSWORD_DEFAULT);
 $sql = "
 INSERT INTO users VALUES(1, 'ez', 'Ghost', 'Rex', '2000-11-06', '12 34 56 78 90', 'pat_rick@gmail.com', '".$password."', 0, '2026-04-27', './assets/background.png', 'Admin', '1');
+INSERT INTO captchas VALUES(1, './assets/captchas/Levis.png);'
 ";
 
 if ($reload) {
