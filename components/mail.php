d075ae1d1e6b0c197ac9671593d98f2372dbce72
CaporalTacos <anthogemu@gmail.com>
2026-06-03 04:42:31 +0200

    mail fix

--- components/mail.php
+++ components/mail.php
@@ -4,6 +4,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 function SendMail($email, $subject, $contenu) {
+
     $env = parse_ini_file('.env');
     $mail = new PHPMailer(true);
 
@@ -28,37 +29,6 @@ function SendMail($email, $subject, $contenu) {
     }
 }
 
-function SendMailNewsletter($users, $subject, $contenu) {
-    $env = parse_ini_file('.env');
-    $mail = new PHPMailer(true);
-
-    try {
-        $mail->isSMTP();
-        $mail->Host= 'smtp.gmail.com';
-        $mail->SMTPAuth= true;
-        $mail->Username = 'noreplytournamento@gmail.com';
-        $mail->Password= $env['SMTP_PASSWORD'];
-        $mail->SMTPSecure= PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port= 587;
-        $mail->SMTPKeepAlive = true;
-
-        $mail->setFrom('noreplytournamento@gmail.com', 'no-replytournamento');
-        $mail->isHTML(true);
-        $mail->Subject = $subject;
-        $mail->Body= $contenu;
-
-        foreach ($users as $user) {
-            $mail->addAddress($user['email_address']);
-            $mail->send();
-            $mail->clearAddresses();
-        }
-
-        $mail->smtpClose();
-    } catch (Exception $e) {
-        displayPageError($mail->ErrorInfo);
-    }
-}
-
 function verifMail($user_id, $email) {
     $token = bin2hex(random_bytes(32));
     $expires = date("Y-m-d H:i:s", time() + 60*60);
@@ -68,6 +38,7 @@ function verifMail($user_id, $email) {
     $stmt = $pdo->prepare("INSERT INTO email_verification (user_id, token, expires_at) VALUES (?, ?, ?)");
     $stmt->execute([$user_id, $token, $expires]);
 
+    $subject = "Vérifier votre compte Tournamento";
     $contenu = "
         <h1>Bienvenue</h1>
         <p>Cliquez ci-dessous pour valider votre compte :</p>
