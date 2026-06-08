7af7fdb805a3a66e8526bb8fb8e31a3051f1844a
Hugo Lemaire <h.lemaire@myskolae.fr>
2026-06-08 16:39:42 +0200

    T36

--- components/security.php
+++ components/security.php
@@ -10,16 +10,7 @@ function verifieRoleAdmin() {
     $user_role = $_SESSION['role'] ?? null;
     
     if ($user_role !== 'Admin') {
-        ?>
-        <?php include_once("./components/header.php"); ?>
-        <div class="denied-container">
-            <h1>Access denied</h1>
-            <button class="denied-button">
-                <a href="<?php echo getPagePath('home') ?>">Retour au site</a>
-            </button>
-        </div>
-        <?php
-        include_once("./components/footer.php");
+        displayPageNotFound();
         exit;
     }
 }
